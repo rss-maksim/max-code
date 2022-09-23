9ea5153f139fcb23634e24a8227c38c349fbf4da
Maksim Kirylka <maksim_kirylka@epam.com>
2022-09-23 11:46:20 +0300

    [leetcode] 845 Longest Mountain in Array

--- src/leetcode/845_longest-mountain-in-array/index.js
+++ src/leetcode/845_longest-mountain-in-array/index.js
@@ -1,7 +1,36 @@
 /**
- * @param {number[]} arr
+ * Complexity:
+ * O(N) time
+ * O(1) space
+ */
+
+/**
+ * @param {number[]} mountain
  * @return {number}
  */
-export const longestMountain = (arr) => {
+export const longestMountain = (mountain) => {
+    let longestLength = 0;
 
+    let i = 1;
+    while (i < mountain.length - 1) {
+        const hasPeak = mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1];
+        if (!hasPeak) {
+            i += 1;
+            continue;
+        }
+        let leftIndex = i - 2;
+        while (leftIndex >= 0 && mountain[leftIndex] < mountain[leftIndex + 1]) {
+            leftIndex -= 1;
+        }
+
+        let rightIndex = i + 2;
+        while (rightIndex < mountain.length && mountain[rightIndex] < mountain[rightIndex - 1]) {
+            rightIndex += 1;
+        }
+
+        longestLength = Math.max(longestLength, rightIndex - leftIndex - 1);
+        i = rightIndex;
+    }
+
+    return longestLength;
 }
