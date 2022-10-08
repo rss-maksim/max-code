8242ac3067f6d7678fd54169c3a855df0568ebb8
Maksim Kirylka <maksim_kirylka@epam.com>
2022-10-08 12:35:16 +0300

    [leetcode] 322. Coin Change. O(coins * amount) time | O(amount) space

---
+++ src/leetcode/322_coin-change/index.js
@@ -0,0 +1,25 @@
+/**
+ * Complexity:
+ * O(coins * amount) time
+ * O(amount) space
+ */
+
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+export const coinChange = (coins, amount) => {
+    const dp = new Array(amount + 1).fill(Number.MAX_VALUE);
+    dp[0] = 0;
+
+    for (const coin of coins) {
+        for (let i = 0; i < dp.length; i++) {
+            if (coin <= i) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
+        }
+    }
+
+    return dp[amount] !== Number.MAX_VALUE ? dp[amount] : -1;
+}
---
+++ src/leetcode/322_coin-change/index.test.js
@@ -0,0 +1,9 @@
+import { coinChange } from './';
+
+test('322. Coin Change problem', () => {
+    expect(coinChange([1,2,5], 11)).toBe(3);
+
+    expect(coinChange([2], 3)).toBe(-1);
+
+    expect(coinChange([1], 0)).toBe(0);
+});
