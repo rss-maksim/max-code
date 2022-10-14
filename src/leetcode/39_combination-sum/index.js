3bd4af2c586d4f14b1115def7a8afb120b470f25
Maksim Kirylka <maksim_kirylka@epam.com>
2022-10-14 13:09:45 +0300

    [leetcode] 39. Combination Sum. O(N^(T/M + 1)) time | O(T/M) space

---
+++ src/leetcode/39_combination-sum/index.js
@@ -0,0 +1,37 @@
+/**
+ * Complexity:
+ * O(n^(T/M + 1)) time
+ * O(T/M) space
+ * `N` the number of candidates, `T` - target value, M - minimum value among the candidates
+ */
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+export const combinationSum = (candidates, target) => {
+    const result = [];
+
+    const backtrack = (remainder, combinations, startIndex) => {
+        if (remainder === 0) {
+            result.push([...combinations]);
+            return;
+        }
+        if (remainder < 0) {
+            return;
+        }
+
+        for (let i = startIndex; i < candidates.length; i++) {
+            combinations.push(candidates[i]);
+
+            backtrack(remainder - candidates[i], combinations, i);
+
+            combinations.pop();
+        }
+    }
+
+    backtrack(target, [], 0);
+
+    return result;
+};
