2f03a53b17bb90e1a976a7d67236599d8f6b3440
Maksim Kirilko <maksimkirilko@gmail.com>
2022-10-29 15:39:35 +0300

    [leetcode] 199. Binary Tree Right Side View. O(n) time | O(H) space

---
+++ src/leetcode/199_binary-tree-right-side-view/index.js
@@ -0,0 +1,37 @@
+/**
+ * Complexity:
+ * O(N) time
+ * O(N) space
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+export const rightSideView = (root) => {
+    if (!root) {
+        return [];
+    }
+    const rightSide = [];
+
+    const rightSideViewHelper = (node, level) => {
+        if (level === rightSide.length) {
+            rightSide.push(node.val);
+        }
+
+        if (node.right) {
+            rightSideViewHelper(node.right, level + 1);
+        }
+
+        if (node.left) {
+            rightSideViewHelper(node.left, level + 1);
+        }
+    }
+
+    rightSideViewHelper(root, 0);
+
+    return rightSide;
+}
+
+
+
---
+++ src/leetcode/199_binary-tree-right-side-view/index.test.js
@@ -0,0 +1,16 @@
+import { rightSideView } from './';
+import { constructBinaryTreeFromSerializedFormat } from './util';
+
+test('543. Diameter of Binary Tree', () => {
+    expect(rightSideView(
+        constructBinaryTreeFromSerializedFormat([1,2,3,null,5,null,4]))
+    ).toEqual([1,3,4]);
+
+    expect(
+        rightSideView(constructBinaryTreeFromSerializedFormat([1,null,3]))
+    ).toEqual([1,3]);
+
+    expect(
+        rightSideView(constructBinaryTreeFromSerializedFormat([]))
+    ).toEqual([]);
+});
