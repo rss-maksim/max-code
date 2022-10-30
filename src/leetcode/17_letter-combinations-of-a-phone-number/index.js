a6f208eaa1dadd543cd032ecfb310f61cb68a065
Maksim Kirilko <maksimkirilko@gmail.com>
2022-10-30 10:45:04 +0200

    [leetcode] 17. Letter Combinations of a Phone Number. O(4^n*n) time | O(n) space

---
+++ src/leetcode/17_letter-combinations-of-a-phone-number/index.js
@@ -0,0 +1,37 @@
+/**
+ * Complexity:
+ * O(4^N * N) time
+ * O(N) space
+ */
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+export const letterCombinations = (digits) => {
+   if (!digits.length) {
+    return [];
+   }
+
+   const combinations = [];
+   const letters = { 2: 'abc', 3: 'def', 4: 'ghi', 5: 'jkl', 6: 'mno', 7:'pqrs', 8: 'tuv', 9: 'wxyz' };
+
+   const backtrack = (index, path) => {
+    if (path.length === digits.length) {
+        combinations.push(path.join(''));
+        return;
+    }
+
+    const possibleLetters = letters[digits[index]];
+
+    for (const letter of possibleLetters) {
+        path.push(letter);
+        backtrack(index + 1, [...path]);
+        path.pop();
+    }
+   }
+
+   backtrack(0, []);
+
+   return combinations;
+}
---
+++ src/leetcode/17_letter-combinations-of-a-phone-number/index.test.js
@@ -0,0 +1,9 @@
+import { letterCombinations } from './';
+
+test('17. Letter Combinations of a Phone Number problem', () => {
+    expect(letterCombinations('23')).toEqual(['ad','ae','af','bd','be','bf','cd','ce','cf']);
+
+    expect(letterCombinations('')).toEqual([]);
+
+    expect(letterCombinations('2')).toEqual(['a', 'b', 'c']);
+});
